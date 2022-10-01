719ea5a260733f5d199ed6e8c61aee0e9a4e69c7
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-10-01 11:06:25 +0200

    Added 2nd homework.

--- Homeworks/Homework_2.py
+++ Homeworks/Homework_2.py
@@ -1,5 +1,5 @@
-children = int(input("How many children are there? "))
 apples = int(input("How many apples do you have? "))
+children = int(input("How many children are there? "))
 
 print("Each child will get", apples // children, "apples.")
 print(apples % children, "apples will remain.")
