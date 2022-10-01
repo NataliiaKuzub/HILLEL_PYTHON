feb88bc2ae10396bb3458f292a3abd1decd127e5
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-10-01 11:03:50 +0200

    Added 2nd homework.

--- Homeworks/Homework_2.py
+++ Homeworks/Homework_2.py
@@ -1,11 +1,5 @@
-valid = False
-while not valid:
-    try:
-        children = int(input("How many children are there? "))
-        apples = int(input("How many apples do you have? "))
-        valid = True
-    except ValueError:
-        print("Please, enter only whole numbers!")
+children = int(input("How many children are there? "))
+apples = int(input("How many apples do you have? "))
 
 print("Each child will get", apples // children, "apples.")
 print(apples % children, "apples will remain.")
