68fc82d8dfc7f3efaf740a4d53fd5f721aa7ed75
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-10-06 17:48:11 +0200

    Edited 3rd homework.

--- Homeworks/Homework_3.py
+++ Homeworks/Homework_3.py
@@ -1,133 +1,28 @@
-while True:
-    valid = False
-    while not valid:
-        try:
-            birth_month = int(input("Please, enter your month of birth (as a number from 1 to 12): "))
-            birth_date = int(input("Please, enter your date of birth: "))
-            valid = True
-        except ValueError:
-            print("Invalid entry. Please, try again.")
-
-    if birth_month > 12 or birth_month < 1 or birth_date < 1:
-        print("Invalid entry. Please, try again.")
-        continue
-    elif birth_month == 1:
-        if birth_date < 20:
-            print("You are Capricorn!")
-            break
-        elif birth_date < 32:
-            print("You are Aquarius!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 2:
-        if birth_date < 19:
-            print("You are Aquarius!")
-            break
-        elif birth_date < 30:
-            print("You are Pisces!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 3:
-        if birth_date < 21:
-            print("You are Pisces!")
-            break
-        elif birth_date < 32:
-            print("You are Aries!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 4:
-        if birth_date < 20:
-            print("You are Aries!")
-            break
-        elif birth_date < 31:
-            print("You are Taurus!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 5:
-        if birth_date < 21:
-            print("You are Taurus!")
-            break
-        elif birth_date < 32:
-            print("You are Gemini!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 6:
-        if birth_date < 21:
-            print("You are Gemini!")
-            break
-        elif birth_date < 31:
-            print("You are Cancer!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 7:
-        if birth_date < 23:
-            print("You are Cancer!")
-            break
-        elif birth_date < 32:
-            print("You are Leo!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 8:
-        if birth_date < 23:
-            print("You are Leo!")
-            break
-        elif birth_date < 32:
-            print("You are Virgo!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 9:
-        if birth_date < 23:
-            print("You are Virgo!")
-            break
-        elif birth_date < 31:
-            print("You are Libra!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 10:
-        if birth_date < 23:
-            print("You are Libra!")
-            break
-        elif birth_date < 32:
-            print("You are Scorpio!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    elif birth_month == 11:
-        if birth_date < 22:
-            print("You are Scorpio!")
-            break
-        elif birth_date < 31:
-            print("You are Sagittarius!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
-    else:
-        if birth_date < 22:
-            print("You are Sagittarius!")
-            break
-        elif birth_date < 32:
-            print("You are Capricorn!")
-            break
-        else:
-            print("Invalid entry. Please, try again.")
-            continue
+birth_month = int(input("Please, enter your month of birth (as a number from 1 to 12): "))
+birth_date = int(input("Please, enter your date of birth: "))
+if (birth_month == 1 and 20 <= birth_date <= 31) or (birth_month == 2 and 1 <= birth_date <= 18):
+    print("You are Aquarius!")
+elif (birth_month == 2 and 19 <= birth_date <= 29) or (birth_month == 3 and 1 <= birth_date <= 20):
+    print("You are Pisces!")
+elif (birth_month == 3 and 21 <= birth_date <= 31) or (birth_month == 4 and 1 <= birth_date <= 19):
+    print("You are Aries!")
+elif (birth_month == 4 and 20 <= birth_date <= 30) or (birth_month == 5 and 1 <= birth_date <= 20):
+    print("You are Taurus!")
+elif (birth_month == 5 and 21 <= birth_date <= 31) or (birth_month == 6 and 1 <= birth_date <= 20):
+    print("You are Gemini!")
+elif (birth_month == 6 and 21 <= birth_date <= 30) or (birth_month == 7 and 1 <= birth_date <= 22):
+    print("You are Cancer!")
+elif (birth_month == 7 and 23 <= birth_date <= 31) or (birth_month == 8 and 1 <= birth_date <= 22):
+    print("You are Leo!")
+elif (birth_month == 8 and 23 <= birth_date <= 31) or (birth_month == 9 and 1 <= birth_date <= 22):
+    print("You are Virgo!")
+elif (birth_month == 9 and 23 <= birth_date <= 30) or (birth_month == 10 and 1 <= birth_date <= 22):
+    print("You are Libra!")
+elif (birth_month == 10 and 23 <= birth_date <= 31) or (birth_month == 11 and 1 <= birth_date <= 22):
+    print("You are Scorpio!")
+elif (birth_month == 11 and 23 <= birth_date <= 30) or (birth_month == 12 and 1 <= birth_date <= 21):
+    print("You are Sagittarius!")
+elif (birth_month == 12 and 22 <= birth_date <= 31) or (birth_month == 1 and 1 <= birth_date <= 19):
+    print("You are Capricorn!")
+else:
+    print("Invalid entry! Please, try again.")
