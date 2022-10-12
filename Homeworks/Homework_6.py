e87d41a44a2baa84706933f3dbf0179796f92f7f
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-10-12 18:59:30 +0200

    Added 6th, 7th and 8th homeworks.

--- Homeworks/Homework_6.py
+++ Homeworks/Homework_6.py
@@ -1,10 +1,6 @@
 text_initial = input("Enter the text please: ")
 text_corrected = ""
 for i in range(len(text_initial)):
-    if text_initial[i] != " ":
+    if text_initial[i] != " " or text_initial[i - 1] != " ":
         text_corrected += text_initial[i]
-    else:
-        if text_initial[i - 1] != " ":
-            text_corrected += text_initial[i]
-
 print(text_corrected)
