6e515133ef967a2c8cef5880773d80ec09ba2777
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-10-13 15:49:26 +0200

    Edited 6th and 7th homeworks.

--- Homeworks/Homework_6.py
+++ Homeworks/Homework_6.py
@@ -1,6 +1,2 @@
 text_initial = input("Enter the text please: ")
-text_corrected = ""
-for i in range(len(text_initial)):
-    if text_initial[i] != " " or text_initial[i - 1] != " ":
-        text_corrected += text_initial[i]
-print(text_corrected)
+print(" ".join(text_initial.split()))
--- Homeworks/Homework_7.py
+++ Homeworks/Homework_7.py
@@ -1,8 +1,5 @@
 text_initial = input("Enter the text, please: ")
 text_corrected = ""
 for i in range(len(text_initial)):
-    if i % 2 == 0:
-        text_corrected += text_initial[i].upper()
-    else:
-        text_corrected += text_initial[i].lower()
+    text_corrected += text_initial[i].upper() if i % 2 == 0 else text_initial[i].lower()
 print(text_corrected)
