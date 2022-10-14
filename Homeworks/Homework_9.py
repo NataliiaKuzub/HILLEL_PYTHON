5136c7da06882db384b68a2ea62f8ad150d44586
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-10-14 15:28:54 +0200

    Edited 9th homework.

--- Homeworks/Homework_9.py
+++ Homeworks/Homework_9.py
@@ -3,4 +3,4 @@ wealth_list = [int(i) for i in wealth.split(", ")]
 print("A silver spoons:")
 for i in range(1, len(wealth_list)-1):
     if wealth_list[i] > wealth_list[i - 1] + wealth_list[i + 1]:
-        print(f"{i+1}. {wealth_list[i]}")
+        print(f"{i}. {wealth_list[i]}")
