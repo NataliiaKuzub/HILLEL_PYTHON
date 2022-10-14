3bab83dc2762f75bd68e1b1f4e6c530279dc9540
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-10-14 15:26:08 +0200

    Added 9th homework.

---
+++ Homeworks/Homework_9.py
@@ -0,0 +1,6 @@
+wealth = (input("Please, enter the salaries of the people separated by coma: "))
+wealth_list = [int(i) for i in wealth.split(", ")]
+print("A silver spoons:")
+for i in range(1, len(wealth_list)-1):
+    if wealth_list[i] > wealth_list[i - 1] + wealth_list[i + 1]:
+        print(f"{i+1}. {wealth_list[i]}")
