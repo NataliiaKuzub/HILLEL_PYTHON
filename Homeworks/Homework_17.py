107c10ea6ceaa5626712f3b54679298571f040d0
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-10-27 15:04:39 +0200

    Added 17th homework.

---
+++ Homeworks/Homework_17.py
@@ -0,0 +1,25 @@
+def dict_constructor(arg_1, arg_2, arg_3, arg_4):
+    return {
+        "model": arg_1,
+        "max_speed": arg_2,
+        "engine": dict(capacity=arg_3, power=arg_4)
+    }
+
+
+def data_list():
+    return input("Please, enter car data (model/speed/capacity/power): ").split("/")
+
+
+def get_item_power(diction):
+    return int(diction["engine"]["power"])
+
+
+cars_data = []
+k = data_list()
+while k != ['']:
+    cars_data.append(dict_constructor(k[0], k[1], k[2], k[3]))
+    k = data_list()
+
+for car in sorted(cars_data, key=get_item_power, reverse=True):
+    print(car)
+
