de51ebdc36a4d176b385d690268621c3c3e6bd81
Nataliia Kuzub <NataliiaKuzub@gmail.com>
2022-11-29 20:02:12 +0100

    Edited 32nd homework.

--- Homeworks/Homework_32.py
+++ Homeworks/Homework_32.py
@@ -19,13 +19,14 @@ class Stack:
     def is_empty(self):
         return len(self.elements) == 0
 
+    @property
     def size(self):
         return len(self.elements)
 
 
 day_planning = Stack(elements=['answer emails', 'go through slack', 'fill the lab journal', 'do experiments'])
 
-print(f'There are {day_planning.size()} activities planned for the day!')
+print(f'There are {day_planning.size} activities planned for the day!')
 
 urgent_things = input('Is there anything else that is urgent? ')
 
@@ -33,7 +34,7 @@ while not urgent_things == '':
     day_planning.push(urgent_things)
     urgent_things = input('Is there anything else that is urgent? ')
 
-print(f"We have {day_planning.size()} activities in the end. Let's start!")
+print(f"We have {day_planning.size} activities in the end. Let's start!")
 state = input("Enter 'ready' when you are ready for a new task. "
               "Enter something else if you can't do this anymore. ")
 
@@ -42,11 +43,11 @@ while state == 'ready' and not day_planning.is_empty:
     state = input()
 
 if not day_planning.is_empty:
-    print(f"Oh, we have not done yet. There are {day_planning.size()} things left.")
+    print(f"Oh, we have not done yet. There are {day_planning.size} things left.")
     saving = input("Enter 'yes' if you want to save undone things for tomorrow. ")
     if saving != 'yes':
         day_planning.clear()
-    print(f'You have {day_planning.size()} things planned for tomorrow.')
+    print(f'You have {day_planning.size} things planned for tomorrow.')
 else:
     print(f"You've done all the things for today, good job!")
 
